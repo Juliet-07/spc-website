498edffaef0daba7aac6fd01fa3c68ef6daee4a1
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2023-12-25 17:12:01 +0100

    building website for spc-universe

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="bg-green-300 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello worldGg!</h1>
+      <p className="tex-sm">dhvbdfivnosivnieufoiwe</p>
+    </div>
+  );
+}
