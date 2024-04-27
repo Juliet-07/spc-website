47015825e95bc923df2814c07933664a39ecc2a4
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2024-04-27 19:02:22 +0200

    mentorship details page

--- src/pages/Community/mentors-network.jsx
+++ src/pages/Community/mentors-network.jsx
@@ -4,14 +4,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { TbArrowBackUp } from "react-icons/tb";
 import Mentors from "../../assets/mentors.png";
 import { RiDoubleQuotesL } from "react-icons/ri";
-import One from "../../assets/people.png";
-import Two from "../../assets/partner.png";
-import Three from "../../assets/method.png";
-import WhyUs from "../../assets/whyUs.png";
-import Pricing from "../../assets/pricing.png";
-import Experience from "../../assets/experience.png";
-import Award from "../../assets/award.png";
-import Team from "../../assets/team.png";
+import Mentor1 from "../../assets/mentor1.png";
+import Mentor2 from "../../assets/mentor2.png";
+import Mentor3 from "../../assets/mentor3.png";
 import Woman from "../../assets/woman.png";
 import Man from "../../assets/man.png";
 import Slider from "react-slick";
@@ -114,48 +109,22 @@ const TestimonialCarousel = () => {
 
 const MentorsNetwork = () => {
   const [modal, setModal] = useState(false);
-  const processess = [
+
+  const mentors = [
     {
-      image: <img src={One} />,
-      title: "People",
-      message:
-        "Our global team of talent experts is ready  to make a real impact on your hiring needs.",
+      image: <img src={Mentor1} />,
+      name: "Lana Stalk",
+      title: "Founder of SSO",
     },
     {
-      image: <img src={Two} />,
-
-      title: "Partnership",
-      message:
-        "We work closely with creative and tech industries to boost growth for global brands, and corporates.",
+      image: <img src={Mentor2} />,
+      name: "Lana Stalk",
+      title: "Founder of SSO",
     },
     {
-      image: <img src={Three} />,
-
-      title: "Methodology",
-      message:
-        "Tailored solutions for your business needs, from talent acquisition to streamlined processes",
-    },
-  ];
-
-  const reasons = [
-    {
-      image: <img src={Pricing} />,
-      text: "Affordable Pricing",
-    },
-    {
-      image: <img src={Experience} />,
-
-      text: "Years of Experience",
-    },
-    {
-      image: <img src={Award} />,
-
-      text: " Award Winning",
-    },
-    {
-      image: <img src={Team} />,
-
-      text: "Professional Team",
+      image: <img src={Mentor3} />,
+      name: "Lana Stalk",
+      title: "Founder of SSO",
     },
   ];
 
@@ -216,34 +185,109 @@ const MentorsNetwork = () => {
           mentors who offer invaluable insights, advice, and support to help
           navigate the complexities of building a successful venture.
         </p>
-        {/* <div className="w-full grid md:grid-cols-2 gap-10 my-10">
-          {talentBenefits.map((talent) => (
-            <div className="md:w-[587px] h-full bg-white border border-[#000000]/[20%] rounded-lg shadow-lg">
-              <div>{talent.image}</div>
-              <p className="text-center py-4 font-primarySemibold text-lg md:text-2xl">
-                {talent.title}
-              </p>
-              <p className="font-primaryRegular px-4 pb-4 md:text-lg leading-8 md:leading-[50px]">
-                {talent.message}
-              </p>
-            </div>
-          ))}
-        </div> */}
+        <div className="w-full grid md:grid-cols-2 2xl:grid-cols-3 gap-10 md:gap-20 my-10">
+          <div>
+            <img src={Community} />
+          </div>
+          <div className="md:w-[516px] h-full flex flex-col items-center">
+            <p className="font-primarySemibold text-2xl text-gray-900">
+              Seasoned Wisdom
+            </p>
+            <p className="font-primaryRegular leading-8 md:leading-[55px]">
+              Our mentor network comprises seasoned entrepreneurs, industry
+              veterans, and subject matter experts with years of hands-on
+              experience in the startup ecosystem. From serial entrepreneurs who
+              have launched and scaled multiple ventures to industry leaders at
+              the forefront of innovation, our mentors bring a wealth of
+              knowledge, wisdom, and perspective to the table.
+            </p>
+          </div>
+          <div className="md:w-[516px] h-full flex flex-col items-center">
+            <p className="font-primarySemibold text-2xl text-gray-900">
+              Tailored Guidance
+            </p>
+            <p className="font-primaryRegular leading-8 md:leading-[55px]">
+              No two startup journeys are alike, which is why our mentorship
+              program is highly personalized and tailored to the unique needs
+              and challenges of each founder. Whether you're grappling with
+              strategic decisions, facing roadblocks in product development, or
+              navigating the intricacies of fundraising, our mentors provide
+              tailored guidance and support to help you overcome obstacles and
+              achieve your goals.
+            </p>
+          </div>
+          <div>
+            <img src={Community} />
+          </div>
+          <div>
+            <img src={Community} />
+          </div>
+          <div className="md:w-[516px] h-full flex flex-col items-center">
+            <p className="font-primarySemibold text-2xl text-gray-900">
+              Inspirational Support
+            </p>
+            <p className="font-primaryRegular leading-8 md:leading-[55px]">
+              Building a startup is not just about overcoming challenges; it's
+              also about staying motivated and inspired in the face of
+              adversity. Our mentors serve as trusted confidants and sources of
+              inspiration, offering encouragement, motivation, and reassurance
+              during the inevitable ups and downs of the entrepreneurial
+              journey. With their unwavering support and belief in your vision,
+              you'll find the courage and resilience to persevere through even
+              the toughest of times.
+            </p>
+          </div>
+          <div className="md:w-[516px] h-full flex flex-col items-center">
+            <p className="font-primarySemibold text-2xl text-gray-900">
+              Network Opportunities
+            </p>
+            <p className="font-primaryRegular leading-8 md:leading-[55px]">
+              Beyond their role as mentors, our network of experienced
+              professionals also serves as invaluable connectors, opening doors
+              to new opportunities, partnerships, and collaborations. Whether
+              it's introductions to potential investors, strategic partners, or
+              industry influencers, our mentors leverage their extensive
+              networks to help founders expand their reach and accelerate their
+              growth.
+            </p>
+          </div>
+          <div>
+            <img src={Community} />
+          </div>
+        </div>
+      </div>
+      {/* mentor team */}
+      <div className="w-full flex flex-col p-4 md:p-10">
+        <div className="md:w-[563px] text-xl md:text-3xl font-primarySemibold">
+          Meet our Creative Team of Mentors and World-Class Problem Solvers
+        </div>
+        <div className="w-full flex items-center justify-center">
+          <div className="w-full grid md:grid-cols-3 gap-10 my-10">
+            {mentors.map((mentor) => (
+              <div className="flex flex-col items-center justify-center">
+                <div>{mentor.image}</div>
+                <p className="text-xl font-primarySemibold">{mentor.name}</p>
+                <p className="font-primaryRegular text-gray-900 italic">
+                  {mentor.title}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       {/* join the community */}
       <div className="w-full flex flex-col p-4 md:p-10 my-4">
-        <div className="font-primarySemibold text-gray-900 text-2xl md:text-4xl text-center md:text-left">
-          <font color="#BE33E0">Join</font> The Community
+        <div className="font-primarySemibold text-gray-900 text-2xl md:text-3xl text-center md:text-left">
+          <font color="#BE33E0">Join The</font> Mentorship Community
         </div>
         <div className="w-full flex flex-col md:flex-row items-center justify-between">
           <div className="md:w-[469px] md:h-[436px] md:text-xl py-4">
             <p className="font-primaryRegular leading-8 md:leading-[50px]">
-              Join the SPC Universe community today and unlock access to a world
-              of opportunity. Whether you're a startup founder in search of your
-              next team member or a talented individual eager to contribute your
-              skills to a groundbreaking venture, your journey begins here.
-              Together, let's build the future of innovation, one collaboration
-              at a time.
+              Join the SPC Universe mentorship community today and gain access
+              to a wealth of knowledge, experience, and support. Whether you're
+              a first-time founder seeking guidance or a seasoned entrepreneur
+              looking to give back, your mentorship journey starts here.
+              Together, let's build a brighter future, one mentorship at a time.
             </p>
             <button
               onClick={() => handleModal()}
@@ -271,7 +315,6 @@ const MentorsNetwork = () => {
           <TestimonialCarousel />
         </div>
       </div>
-
       <Modal isVisible={modal} onClose={() => setModal(false)}>
         <div className="flex flex-col items-center justify-center">
           <StartupForm />
